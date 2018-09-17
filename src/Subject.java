6de2fea745a3c694494ce593103f4fee10c45c49
gavinev98 <gavin.everett48@hotmail.com>
2018-09-17 21:48:59 +0100

    Added Subject Class

---
+++ src/Subject.java
@@ -0,0 +1,8 @@
+import java.util.Observer;
+
+public interface Subject {
+
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObserver(Observer o);
+}
